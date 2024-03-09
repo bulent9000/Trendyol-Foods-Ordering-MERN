d8215b90d6f4da3331b1061a43fc4afb3518ffc9
Bülent Karaarslan <137310737+bulent9000@users.noreply.github.com>
2024-03-09 17:11:13 +0300

    MongoDb database connection provided

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,11 @@
 import express,{Request,Response} from "express";
 import cors from "cors";
 import "dotenv/config";
-import { log } from "console";
+import mongoose from "mongoose";
 
+
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(()=>console.log("Database connected successfully")
+);
 const app =express();
 app.use(express.json());
 app.use(cors());
@@ -13,6 +16,6 @@ res.json({message:"Hello"});
 
 app.listen(7000,()=>{
 
-console.log("Connected successfully");
+
 
 })
