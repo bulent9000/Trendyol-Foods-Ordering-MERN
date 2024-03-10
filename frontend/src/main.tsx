829b4d3a4e1f4e73b5260e423d71a0c2f074dcd3
Bülent Karaarslan <137310737+bulent9000@users.noreply.github.com>
2024-03-10 14:08:31 +0300

    Header component created and snippet set

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom';
 import AppRoutes from './AppRoutes';
-import './index.css'
+import './global.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
