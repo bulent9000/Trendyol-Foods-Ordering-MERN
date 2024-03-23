df6a11f348a09362858ff6e963e422829e8ce325
Bülent Karaarslan <137310737+bulent9000@users.noreply.github.com>
2024-03-23 16:09:10 +0300

    Creating a restaurant addition form for each user and using the zod validation library

---
+++ frontend/src/pages/ManageRestaurantPage.tsx
@@ -0,0 +1,8 @@
+import ManageRestaurantForm from "@/forms/manage-restaurant-form/ManageRestaurantForm"
+
+const ManageRestaurantPage=()=>{
+    return (
+        <ManageRestaurantForm></ManageRestaurantForm>
+    )
+}
+export default ManageRestaurantPage;
