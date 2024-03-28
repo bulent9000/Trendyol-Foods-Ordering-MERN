61741767d4a0d1b62c27923da30f96ae7e3cb7d5
Bülent Karaarslan <137310737+bulent9000@users.noreply.github.com>
2024-03-28 19:38:45 +0300

    Create Searchbar component and routing to the restaurants which belong the city

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,23 +1,40 @@
 import landing from "../assets/landing.png";
 import appDownload from "../assets/appDownload.png";
-const HomePage=()=>{
-return(
+import SearchBar, { SearchForm } from "@/components/SearchBar";
+import { useNavigate } from "react-router-dom";
+const HomePage = () => {
+  const navigate = useNavigate();
+  const handleSearchSubmit = (searchFormValues: SearchForm) => {
+    navigate({
+      pathname: `/search/${searchFormValues.searchQuery}`,
+    });
+  };
+
+  return (
     <div className="flex flex-col gap-12">
-        <div className="bg-white rounded-lg shadow-md py-8 flex flex-col gap-5 text-center -mt-16">
+      <div className="md:px-32 bg-white rounded-lg shadow-md py-8 flex flex-col gap-5 text-center -mt-16">
+        <h1 className="text-5xl font-bold tracking-tight text-orange-600">
+          Tuck into a take away now
+        </h1>
+        <span className="text-xl">Food is just a click away</span>
 
-            <h1 className="text-5xl font-bold tracking-tight text-orange-600">Tuck into a take away now</h1>
-            <span className="text-xl">Food is just a click away</span>
+        <SearchBar
+          placeHolder="Search by City or Town"
+          onSubmit={handleSearchSubmit}
+        />
+      </div>
+      <div className="grid md:grid-cols-2 gap-5">
+        <img src={landing} />
+        <div className="flex flex-col items-center justify-center gap-4 text-center">
+          <span className="font-bold text-3xl tracking-tighter">
+            Order takeaway even faster
+          </span>
+          <span>Download the Trendyol App for faster ordering</span>
+          <img src={appDownload} />
         </div>
-        <div className="grid md:grid-cols-2 gap-5">
-        <img src={landing}/>
-        <div className="flex flex-col items-center justify-center gap-4 text-center" ><span className="font-bold text-3xl tracking-tighter">Order takeaway even faster</span>
-        <span>Download the Trendyol App for faster ordering</span>
-        <img src={appDownload} />
-        </div>
-        </div>
+      </div>
     </div>
-)
-
-}
+  );
+};
 
 export default HomePage;
