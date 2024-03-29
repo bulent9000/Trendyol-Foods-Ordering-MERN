b7dd8424d7c3103dec742b9b67044848c0ec6fbb
Bülent Karaarslan <137310737+bulent9000@users.noreply.github.com>
2024-03-29 18:59:47 +0300

    Search for Api request

--- frontend/src/pages/SearchPage.tsx
+++ frontend/src/pages/SearchPage.tsx
@@ -1,10 +1,16 @@
+import { useSearchRestaurants } from "@/api/RestaurantApi";
 import { useParams } from "react-router-dom";
 
 const SearchPage = () => {
   const {city}=useParams();
+  const {results}=useSearchRestaurants(city);
+
   return(
 
-    <span>User search for {city}</span>
+    <span>User search for {city} 
+    {results?.data.map((restaurant)=>(<span>
+      found - {restaurant.restaurantName}, {restaurant.city}    </span>))}
+    </span>
   )
 }
 
--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -28,3 +28,12 @@ imageUrl:string,
 lastUpdated:string,
 
 }
+
+export type RestaurantSearchResponse={
+    data:Restaurant[];
+    pagination:
+    {
+    total:number,
+    page:number,
+    pages:number,}
+}
