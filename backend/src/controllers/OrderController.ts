11fab3505afde39787bf49e0d2d37ec46a415690
Bülent Karaarslan <137310737+bulent9000@users.noreply.github.com>
2024-04-07 01:04:03 +0300

    Start stripe Cli with nodemon

--- backend/src/controllers/OrderController.ts
+++ backend/src/controllers/OrderController.ts
@@ -20,6 +20,13 @@ type CheckoutSessionRequest = {
   restaurantId: string;
 };
 
+const stripeWebhookHandler = async (req: Request, res: Response) => {
+  console.log("RECEIVED EVENT");
+  console.log("==============");
+  console.log("event", req.body);
+  res.send();
+};
+
 const createCheckoutSession = async (req: Request, res: Response) => {
   try {
     const checkoutSessionRequest: CheckoutSessionRequest = req.body;
@@ -123,4 +130,5 @@ const createSession = async (
 
 export default {
   createCheckoutSession,
+  stripeWebhookHandler,
 };
