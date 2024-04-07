a74a3d6e0d2f528120de4c12271ce0f67d08ccab
Bülent Karaarslan <137310737+bulent9000@users.noreply.github.com>
2024-04-07 14:49:22 +0300

    Update order with using stripe webhook and sending order status and total amount from frontend to stripe and from stripe to backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,38 +1,35 @@
-import express,{Request,Response} from "express";
+import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
 import myUserRoute from "./routes/MyUserRoutes";
-import {v2 as cloudinary} from "cloudinary";
+import { v2 as cloudinary } from "cloudinary";
 import myRestaurantRoute from "./routes/MyRestaurantRoute";
-import restaurantRoute from "./routes/RestaurantRoute"
+import restaurantRoute from "./routes/RestaurantRoute";
 import orderRoute from "./routes/OrderRoute";
 
-mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(()=>console.log("Database connected successfully")
-);
- 
+mongoose
+  .connect(process.env.MONGODB_CONNECTION_STRING as string)
+  .then(() => console.log("Database connected successfully"));
+
 cloudinary.config({
-  cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
-  api_key:process.env.CLOUDINARY_API_KEY,
-  api_secret:process.env.CLOUDINARY_API_SECRET,
-})
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
-
-const app =express();
+const app = express();
 
 app.use(cors());
+//For security and validation,access in the request raw data
+app.use("/api/order/checkout/webhook", express.raw({ type: "*/*" }));
 app.use(express.json());
 app.get("/health", async (req: Request, res: Response) => {
-    res.send({ message: "health OK!" });
-  });
-app.use("/api/my/user",myUserRoute);
-app.use("/api/my/restaurant",myRestaurantRoute);
-app.use("/api/restaurant",restaurantRoute);
-app.use("/api/order",orderRoute);
+  res.send({ message: "health OK!" });
+});
+app.use("/api/my/user", myUserRoute);
+app.use("/api/my/restaurant", myRestaurantRoute);
+app.use("/api/restaurant", restaurantRoute);
+app.use("/api/order", orderRoute);
 
-
-app.listen(7000,()=>{
-
-
-
-})
+app.listen(7000, () => {});
