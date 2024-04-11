25efcaef5627bf0b2fdfbd431013a11ecfe486d9
Bülent Karaarslan <137310737+bulent9000@users.noreply.github.com>
2024-04-11 18:22:50 +0300

    Order status page created for logged in user

--- frontend/src/components/MobileNav.tsx
+++ frontend/src/components/MobileNav.tsx
@@ -12,21 +12,35 @@ import { useAuth0 } from "@auth0/auth0-react";
 import MobileNavLinks from "./MobileNavLinks";
 
 const MobileNav = () => {
-  const {isAuthenticated,loginWithRedirect,user}=useAuth0();
+  const { isAuthenticated, loginWithRedirect, user } = useAuth0();
   return (
     <Sheet>
       <SheetTrigger>
         <Menu className="text-orange-500" />
       </SheetTrigger>
       <SheetContent className="space-y-3">
-        <SheetTitle>{isAuthenticated?(<span className="flex items-center font-bold gap-2"><CircleUserRound className="text-orange-500"/>
-        {user?.email}</span>): ( <span>Trendyol Go</span>)}
-        
+        <SheetTitle>
+          {isAuthenticated ? (
+            <span className="flex items-center font-bold gap-2">
+              <CircleUserRound className="text-orange-500" />
+              {user?.email}
+            </span>
+          ) : (
+            <span>Trendyol Go</span>
+          )}
         </SheetTitle>
         <Separator />
         <SheetDescription className="flex flex-col gap-4">
-          {isAuthenticated? <MobileNavLinks/> :  <Button onClick={()=>loginWithRedirect()} className="flex-1 font-bold bg-orange-500">LOG IN</Button> }
-        
+          {isAuthenticated ? (
+            <MobileNavLinks />
+          ) : (
+            <Button
+              onClick={() => loginWithRedirect()}
+              className="flex-1 font-bold bg-orange-500"
+            >
+              LOG IN
+            </Button>
+          )}
         </SheetDescription>
       </SheetContent>
     </Sheet>
